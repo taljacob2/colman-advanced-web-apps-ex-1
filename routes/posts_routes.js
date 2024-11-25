409c9e056221bacc596063c7669ffa3c4c408b12
Tal Jacob <taljacob2@gmail.com>
2024-11-25 21:19:07 +0200

    Update routes/posts_routes.js

--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../controllers/post');
-
+const Post = require('../controllers/posts_controller');
 router.get('/', Post.getAllPosts);
 
 router.post('/', Post.createPost);
