7af6e569336dc024c1e6cde72987b5990fbd8bd1
Tal Jacob <taljacob2@gmail.com>
2024-11-30 11:24:50 +0200

    Handle Error When JSON Syntax Given Is Not Parseable
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Error handler for invalid JSON
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError) {
+        return res.status(400).send('Invalid JSON syntax');
+    }
+    next(err);
+});
+
 const posts_routes = require('./routes/posts_routes');
 app.use('/post', posts_routes);	
 
