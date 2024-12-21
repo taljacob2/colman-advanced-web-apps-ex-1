fdd7fbad1730868170d130a34cde568b95e38482
Tal Jacob <taljacob2@gmail.com>
2024-12-21 16:45:49 +0200

    Add POST /post And GET /post Tests
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- tests/__tests__/controllers/posts_controller.test.js
+++ tests/__tests__/controllers/posts_controller.test.js
@@ -1,15 +1,28 @@
 const request = require('supertest');
 const app = require('../../../app');
 
+describe('given db empty of posts when http request GET /post', () => {
+    it('then should return empty list', async () => {
+        const res = await request(app).get('/post');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toStrictEqual([]);
+    });
+});
+
 describe('given empty db when http request POST /post', () => {
     it('then should add posts to the db', async () => {
+        const body = {
+            "sender": "Tal",
+            "title": "Post 2",
+            "content": "HELLO 12345!!"
+        };
         const res = await request(app).post('/post')
-            .send({
-                "sender": "Tal",
-                "title": "Post 2",
-                "content": "HELLO 12345!!"
-            });
+            .send(body);
+        const resBody = res.body;
+        delete resBody._id;
+
         expect(res.statusCode).toBe(201);
+        expect(resBody).toEqual(body);
     });
 });
 
@@ -17,5 +30,6 @@ describe('given db initialized with posts when http request GET /post', () => {
     it('then should return all posts in the db', async () => {
         const res = await request(app).get('/post');
         expect(res.statusCode).toBe(200);
+        expect(res.statusCode).not.toEqual([]);
     });
 });
