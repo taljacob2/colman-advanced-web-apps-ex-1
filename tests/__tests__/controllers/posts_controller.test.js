b1cb050f46997121f8412ae000d8edf9a8e8cc4a
Tal Jacob <taljacob2@gmail.com>
2024-12-21 17:13:01 +0200

    Add Posts Tests
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- tests/__tests__/controllers/posts_controller.test.js
+++ tests/__tests__/controllers/posts_controller.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../../../app');
 
+let exitingPost = null;
+
 describe('given db empty of posts when http request GET /post', () => {
     it('then should return empty list', async () => {
         const res = await request(app).get('/post');
@@ -9,20 +11,34 @@ describe('given db empty of posts when http request GET /post', () => {
     });
 });
 
-describe('given empty db when http request POST /post', () => {
-    it('then should add posts to the db', async () => {
+describe('when http request POST /post', () => {
+    it('then should add post to the db', async () => {
         const body = {
-            "sender": "Tal",
-            "title": "Post 2",
-            "content": "HELLO 12345!!"
+            "sender": "USERNAME",
+            "title": "POST TITLE",
+            "content": "POST CONTENT"
         };
         const res = await request(app).post('/post')
             .send(body);
         const resBody = res.body;
+        exitingPost = { ...resBody };
         delete resBody._id;
 
         expect(res.statusCode).toBe(201);
         expect(resBody).toEqual(body);
+    });
+});
+
+describe('when http request POST /post without required sender field', () => {
+    it('then should return 400 bad request http status', async () => {
+        const body = {
+            "title": "POST TITLE",
+            "content": "POST CONTENT"
+        };
+        const res = await request(app).post('/post')
+            .send(body);
+
+        expect(res.statusCode).toBe(400);
     });
 });
 
@@ -31,5 +47,61 @@ describe('given db initialized with posts when http request GET /post', () => {
         const res = await request(app).get('/post');
         expect(res.statusCode).toBe(200);
         expect(res.statusCode).not.toEqual([]);
+    });
+});
+
+describe('given unknown username when http request GET /post?sender', () => {
+    it('then should return empty list', async () => {
+        const res = await request(app).get('/post?sender=UNKNOWN');
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual([]);
+    });
+});
+
+describe('given known username when http request GET /post?sender', () => {
+    it('then should return his posts only', async () => {
+        const res = await request(app).get('/post?sender=USERNAME');
+        const resBody = res.body;
+        const senderList = resBody.map((post) => post.sender);
+
+        // Use filter and return array with unique values
+        const uniqueSenderList = senderList.filter(
+            (e, i, self) => i === self.indexOf(e));
+
+        expect(res.statusCode).toBe(200);
+        expect(uniqueSenderList.length).toBe(1);
+        expect(uniqueSenderList[0]).toEqual(resBody[0].sender);
+    });
+});
+
+describe('when http request PUT /post/id of unknown post', () => {
+    it('then should return 400 bad request http status', async () => {
+        const body = {
+            "sender": "UPDATED USERNAME",
+            "title": "UPDATED POST TITLE",
+            "content": "UPDATED POST CONTENT"
+        };
+        const res = await request(app).put(`/post/UNKNOWN`)
+            .send(body);
+
+        expect(res.statusCode).toBe(400);
+    });
+});
+
+describe('when http request PUT /post/id of existing post', () => {
+    it('then should update post in the db', async () => {
+        const body = {
+            "sender": "UPDATED USERNAME",
+            "title": "UPDATED POST TITLE",
+            "content": "UPDATED POST CONTENT"
+        };
+        const res = await request(app).put(`/post/${exitingPost._id}`)
+            .send(body);
+        const resBody = res.body;
+        delete resBody._id;
+
+        expect(res.statusCode).toBe(201);
+        expect(resBody).toEqual(body);
     });
 });
